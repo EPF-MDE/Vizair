4e14c419856488d56031043bbd54285f043779c8
Tim-EPF <tim.frankenmolen@epfedu.fr>
2021-03-02 17:04:03 +0100

    Revert "Add files via upload"
    This reverts commit 0425540949ac79426b29aaf58a97cbae825a4d71.

--- sources/hello.py
+++ sources/hello.py
@@ -1,1 +1,2 @@
-print('hello {0}'.format('Melisa')) 
+print('hello i\'m tim v2')
+print('hello {0}'.format('Yvan LORRAIN')) # captain obvious : yes, replace this string with your first and last name !
