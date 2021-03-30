804ca686e1c63f5be1f152c617b5d729a7696ba8
AudreyBianchi <64652405+AudreyBianchi@users.noreply.github.com>
2021-03-30 16:56:31 +0200

    Delete TP3-TestPython-AudreyBianchi directory

--- TP3-TestPython-AudreyBianchi/sources/math.py
+++
@@ -1,8 +0,0 @@
-def add(a,b):
-    return 5
-
-def fibo(n):                                                                                                 
-    a, b = 1, 1
-    for i in range(1, n+1):
-        a, b = b, b+a
-    return a
